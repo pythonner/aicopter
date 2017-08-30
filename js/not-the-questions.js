4036a105542aee03dc9691a81a801fcb2f0b406b
David Nadeau <david.nadeau@gmail.com>
2017-08-30 14:52:51 -0400

    Copter game is back for AI and Publishers

---
+++ js/not-the-questions.js
@@ -0,0 +1,139 @@
+/**
+ * Shhhhhhh
+ */
+
+var secretVariable = {
+
+    questions : [
+        {
+            label1 : "Alan Turing", 
+            label2 : "George Turing", 
+            correct : "c342cdb7d4ad65f3467f1ab056f1f9c6"
+        },
+        {
+            label1 : "Rights management", 
+            label2 : "Wrong management", 
+            correct : "fa5b75b5cb023b0fce86d95422651c91"
+        },
+        {
+            label1 : "Decision tree", 
+            label2 : "Decision paradox", 
+            correct : "bad4e6e3aca79beffb4143c037da24da"
+        },
+        {
+            label1 : "Impress", 
+            label2 : "Imprint", 
+            correct : "e206d098296c1966e2d01130f9195744"
+        },
+        {
+            label1 : "Neural networks", 
+            label2 : "Net neutrality", 
+            correct : "6b347be0e79381eeb5689396d9e59438"
+        },
+        {
+            label1 : "Survival kit", 
+            label2 : "Media kit", 
+            correct : "2af3e80a97a19c18598cb678a29f24d2"
+        },
+        {
+            label1 : "Reinforcement Learning", 
+            label2 : "Enforcement Learning", 
+            correct : "44f8995701f5e6cc074e161e0e2f6f24"
+        },
+        {
+            label1 : "Press release", 
+            label2 : "Press and release", 
+            correct : "88ef5f67226ef5aeea5556fa7380a185"
+        },
+        {
+            label1 : "Data mining", 
+            label2 : "Sustainable mining", 
+            correct : "d0ee2a28a1b3c58020717a59e9b14ea6"
+        },
+        {
+            label1 : "Frankfurt weather", 
+            label2 : "Frankfurt Book Fair", 
+            correct : "a09449fed192fa2f7cdfaf042e306358"
+        },
+        {
+            label1 : "Sequence labelling", 
+            label2 : "Label sequencing", 
+            correct : "c4c461b13b3f9fa5d57ee158179512c1"
+        },
+        {
+            label1 : "Members of Congress", 
+            label2 : "Library of Congress", 
+            correct : "6aa79095b296890019410d8884b7d981"
+        },
+        {
+            label1 : "Random forest", 
+            label2 : "Empty forest", 
+            correct : "64caf2dbf3c361d4689629bdc10ab2ca"
+        },
+        {
+            label1 : "Spot varnish", 
+            label2 : "Spot on", 
+            correct : "3464872dbccf5bf0951f0f131295de93"
+        },
+        {
+            label1 : "Genetic algorithm", 
+            label2 : "Generic engineering", 
+            correct : "7169b3c688b0af1fc8a460deaad6df3d"
+        },
+        {
+            label1 : "Evidence", 
+            label2 : "Proof", 
+            correct : "85d2c1a86d34d86b5b30f6286e1f13df"
+        },
+        {
+            label1 : "Cross-validation", 
+            label2 : "Crossfit", 
+            correct : "4f86301e476c9eb1941e35729269604b"
+        },
+        {
+            label1 : "Quantum duck", 
+            label2 : "Quark Xpress", 
+            correct : "7b56f33ed70b0a334ddb5f0a69e450d4"
+        },
+        {
+            label1 : "Reverend Bayes", 
+            label2 : "Naive Bayes", 
+            correct : "952bdf11eb2e40df2b51460a15180889"
+        },
+        {
+            label1 : "Folio", 
+            label2 : "Polio", 
+            correct : "8292553558a75e672bc62e5a84244c82"
+        },
+        {
+            label1 : "Overfitting", 
+            label2 : "Overtraining", 
+            correct : "6c8be24f8b2df78c4828c5ad07701078"
+        },
+        {
+            label1 : "ABCD", 
+            label2 : "ISBN", 
+            correct : "9609dddf3618cff8f67b7829e6fc575e"
+        },
+        {
+            label1 : "Artificial intelligence", 
+            label2 : "Natural stupidity", 
+            correct : "9d0996a44c6d51cf223e833dceecb286"
+        },
+        {
+            label1 : "Typesetting", 
+            label2 : "Settings and storage", 
+            correct : "7332dcfb985772f3e13843e986c3adf5"
+        },
+        {
+            label1 : "Innodata", 
+            label2 : "Go up!", 
+            correct : "2ee9570a6d86d2975cd2c60b84bafcb1"
+        },
+        {
+            label1 : "The", 
+            label2 : "End", 
+            correct : ""
+        }
+    ]
+}
